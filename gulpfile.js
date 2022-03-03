3d5bbdee053216a7fa9985c1e6ed6ba945686c91
topgirl2410 <maria.topgirl82@gmail.com>
2022-03-03 01:32:47 +0100

    convirtiendo imagenes en webp

--- gulpfile.js
+++ gulpfile.js
@@ -8,36 +8,42 @@ const autoprefixer = require('autoprefixer');
 
 // Imagenes
 const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
 
-function css( done ) {
+
+function css(done) {
     src('src/scss/app.scss')
-        .pipe( sass() )
-        .pipe( postcss([ autoprefixer() ]) )
-        .pipe( dest('build/css') )
+        .pipe(sass())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(dest('build/css'))
 
     done();
 }
 
 function imagenes() {
     return src('src/img/**/*')
-        .pipe( imagemin({ optimizationLevel: 3 }) )
-        .pipe( dest('build/img') )
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest('build/img'))
 }
 
-
+function versionWebp() {
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp())
+        .pipe(dest('build/img'))
+}
 
 function dev() {
-    watch( 'src/scss/**/*.scss', css );
-    watch( 'src/img/**/*', imagenes );
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
 }
 
 
 exports.css = css;
 exports.dev = dev;
 exports.imagenes = imagenes;
-
-exports.default = series( imagenes, css, dev  );
+exports.versionWebp = versionWebp;
+exports.default = series(imagenes, versionWebp, css, dev);
 
 // series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente
 // parallel - Todas inician al mismo tiempo
