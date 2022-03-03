19a514d2dce179ce154d5b6b5a040d62b85647cf
topgirl2410 <maria.topgirl82@gmail.com>
2022-03-03 01:02:34 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const { src, dest, watch, series, parallel } = require('gulp');
+
+// CSS y SASS
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+
+// Imagenes
+const imagemin = require('gulp-imagemin');
+
+
+function css( done ) {
+    src('src/scss/app.scss')
+        .pipe( sass() )
+        .pipe( postcss([ autoprefixer(), cssnano() ]) )
+        .pipe( sourcemaps.write('.'))
+        .pipe( dest('build/css') )
+
+    done();
+}
+
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe( imagemin({ optimizationLevel: 3 }) )
+        .pipe( dest('build/img') )
+}
+
+
+
+function dev() {
+    watch( 'src/scss/**/*.scss', css );
+    watch( 'src/img/**/*', imagenes );
+}
+
+
+exports.css = css;
+exports.dev = dev;
+exports.imagenes = imagenes;
+
+exports.default = series( imagenes, css, dev  );
+
+// series - Se inicia una tarea, y hasta que finaliza, inicia la siguiente
+// parallel - Todas inician al mismo tiempo
